7a69483b2b0c271970d259b6f75c67c87b816d11
recre4te <avivaldi222@gmail.com>
2019-03-25 10:42:36 +0300

    Initial Commit

---
+++ PictureTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PictureTableView
+//
+//  Created by Mark on 25/03/2019.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
